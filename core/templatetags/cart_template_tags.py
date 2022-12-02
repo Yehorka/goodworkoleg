e0bab48b1ee6dcb6bd400e5e43d3a3502bc4cdab
Yehorka <25kabachok25@gmail.com>
2022-12-02 14:47:50 +0200

    Cart is working!

--- core/templatetags/cart_template_tags.py
+++ core/templatetags/cart_template_tags.py
@@ -1,6 +1,6 @@
 from django import template
 from core.models import Order
-
+from django.utils.safestring import mark_safe
 register = template.Library()
 
 
@@ -10,4 +10,38 @@ def cart_item_count(user):
         qs = Order.objects.filter(user=user, ordered=False)
         if qs.exists():
             return qs[0].items.count()
+    return 0
+
+@register.filter
+def cart_item_price(user):
+    if user.is_authenticated:
+        qs = Order.objects.get(user=user, ordered=False)
+        total = 0
+        for order_item in qs.items.all():
+            total += order_item.get_total_item_price()
+        return total
+    return 0
+
+@register.filter
+def get_cart(user):
+    if user.is_authenticated:
+        order = Order.objects.get(user=user, ordered=False)
+        total = ""
+        for order_item in order.items.all():
+            total += """<li class="header-cart-item">
+									<div class="header-cart-item-img">
+										<img src="{}" alt="IMG">
+									</div>
+
+									<div class="header-cart-item-txt">
+										<a href="#" class="header-cart-item-name">
+											{}
+										</a>
+
+										<span class="header-cart-item-info">
+											{} x ${}
+										</span>
+									</div>
+								</li>""".format(order_item.item.image.url, order_item.item.title, order_item.quantity, order_item.item.price)
+        return mark_safe(total)
     return 0
